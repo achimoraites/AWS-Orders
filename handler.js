9b2ada805b4fce90b6d77c9ef9d0d10bb9435c51
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-03 18:33:07 +0200

    added comments

--- handler.js
+++ handler.js
@@ -27,27 +27,28 @@ module.exports.newOrder = async event => {
         order_id
       }
     }).promise();
-    // if the item exists stop!
+
+    // if the order already exists stop!
     if("Item" in exists) {
       throw new Error(`Order ${order_id}, already exists in store ${store_id}`);
     }
     // add the order in the StoreOrders table
     await dynamoDC.put(params(store_id, order_id, user_id, grand_total)).promise();
+    // successful response
     return {
       statusCode: 200,
-      exists,
       body: {
         message: `Order ${order_id} submitted successfully`,
       },
     
     };
   } catch (error) {
+    // error has occured !
     return {
       statusCode: 400,
       body: 'request failed',
       error : error.message
     };   
   }
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
 };
