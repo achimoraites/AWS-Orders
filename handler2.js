c832f9859913649253583bb6fe85fa00831cbb8d
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-04 14:39:57 +0200

    refactored scan to a query for better performance

--- handler2.js
+++ handler2.js
@@ -13,14 +13,16 @@ module.exports.userOrders = async event => {
     const { user_id } = event;
     const params=  {
       TableName:'StoreOrders',
-      FilterExpression:'user_id = :ui',
-      ExpressionAttributeValues:{ ":ui" : user_id }
+      IndexName: 'user_id-timestamp-index',
+      KeyConditionExpression: "user_id = :ui",
+      ExpressionAttributeValues: {
+        ":ui": user_id
+      }
     };
    
     console.log(user_id);
-    // It has to be a better way!
     // this solution works but is labor intensive 
-    const res = await dynamoDC.scan(params).promise();
+    const res = await dynamoDC.query(params).promise();
     // if there are no results stop here
     if (res.Count === 0) {
       throw new Error(`User ${user_id} has no orders yet !`);
