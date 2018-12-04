32dea818ad37feb57f579fe1599fef06b44925af
Achilles Moraites <achimoraites@yahoo.gr>
2018-12-04 12:03:33 +0200

    userOrders works but needs a better solution

--- handler2.js
+++ handler2.js
@@ -2,6 +2,7 @@
 const AWS = require('aws-sdk');
 const dynamoDC = new AWS.DynamoDB.DocumentClient({});
 
+
 module.exports.userOrders = async event => {
 
 
@@ -9,19 +10,21 @@ module.exports.userOrders = async event => {
     // ... The results should be the last 5 orders by the given user
 
     // Get users orders using user_id
-    // Return 5 most recent
     const { user_id } = event;
     const params=  {
       TableName:'StoreOrders',
       FilterExpression:'user_id = :ui',
-      Limit: 5,
       ExpressionAttributeValues:{ ":ui" : user_id }
     };
    
-   
     console.log(user_id);
-    const orders = await dynamoDC.scan(params).promise();
-
+    // It has to be a better way!
+    // this solution works but is labor intensive 
+    const res = await dynamoDC.scan(params).promise();
+    // sort by timestamp
+    const orders2 = res.Items.sort((a, b)=> a.timestamp < b.timestamp);
+    //  5 most recent orders
+    const orders = orders2.slice(0, 5);  
     // successful response
     return {
       statusCode: 200,
